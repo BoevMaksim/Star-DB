dbc1b63566b0cd61d3ce818fa7a683c1776153b5
BoevMaksim <maksim.schetina@gmail.com>
2019-10-31 14:30:45 +0300

    implemented Spinner component (base-style)

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -8,22 +8,18 @@ const RandomPlanet = () => {
 
     const swapiService = new SwapiService();
 
-    const [id, setId] = useState(null);
-    const [name, setName] = useState(null);
-    const [population, setPopulation] = useState(null);
-    const [rotationPeriod, setRotationPeriod] = useState(null);
-    const [diameter, setDiameter] = useState(null);
+    const [planet, setPlanet] = useState({});
+
+    const onPlanetLoaded = (planet) => {
+        setPlanet(planet);
+    };
+
+    const {id, name, diameter, population, rotationPeriod} = planet;
 
     const updatePlanet = (id) => {
         swapiService
             .getPlanet(id)
-            .then((planet) => {
-                setId(id);
-                setName(planet.name);
-                setDiameter(planet.diameter);
-                setPopulation(planet.population);
-                setRotationPeriod(planet.rotation_period);
-            });
+            .then(onPlanetLoaded);
     };
 
     useEffect(() => {
