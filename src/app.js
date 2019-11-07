f1a5ccd2cf6142119d636c9f5cb62d7d2df3cef7
BoevMaksim <maksim.schetina@gmail.com>
2019-11-07 09:56:07 +0300

    implemented People-page component

--- src/app.js
+++ src/app.js
@@ -1,32 +1,20 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import Header from './components/header';
 import RandomPlanet from './components/random-planet';
-import ItemList from './components/item-list';
-import PersonDetails from './components/person-details';
 
 import './app.css';
 
+import PeoplePage from "./components/people-page/people-page";
+
 const App = () => {
 
-    const [selectedPerson, setSelectedPerson] = useState(1);
-
-    const onPersonSelected = (id) => {
-        setSelectedPerson(id);
-    };
   return (
     <div>
       <Header />
       <RandomPlanet />
+      <PeoplePage />
 
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList onItemSelected={onPersonSelected} />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails personId={selectedPerson}/>
-        </div>
-      </div>
     </div>
   );
 };
